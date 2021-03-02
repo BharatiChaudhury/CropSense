db54f0ed11bd6ae48fa49bae80912b4bb9f48266
Shashvat Gupta <shashvat6264@gmail.com>
2021-03-02 17:03:18 +0530

    Masking Done

--- app/src/main/java/com/example/myapplication/DrawCircleActivity.java
+++ app/src/main/java/com/example/myapplication/DrawCircleActivity.java
@@ -4,7 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -13,7 +19,8 @@ public class DrawCircleActivity extends AppCompatActivity {
     private Button backBtn;
     private Button nextBtn;
     private ImageView img;
-    private Bitmap bmp = RotateActivity.getBmp();
+    private float radius = SelectParametersActivity.getImgRad();
+    private Bitmap bmp = getCircledBitmap(RotateActivity.getBmp());
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +32,7 @@ public class DrawCircleActivity extends AppCompatActivity {
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(DrawCircleActivity.this,MainActivity.class);
+                Intent intent = new Intent(DrawCircleActivity.this, MainActivity.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(intent);
             }
@@ -33,9 +40,24 @@ public class DrawCircleActivity extends AppCompatActivity {
         nextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(DrawCircleActivity.this,PolygonActivity.class);
+                Intent intent = new Intent(DrawCircleActivity.this, PolygonActivity.class);
                 startActivity(intent);
             }
         });
+    }
+
+    public Bitmap getCircledBitmap(Bitmap bitmap) {
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        canvas.drawCircle(bitmap.getWidth() / 2, bitmap.getHeight() / 2, radius*2, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+
+        return output;
     }
 }
